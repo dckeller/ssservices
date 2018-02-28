c432f42fcf71cee077543dbcf69e800975bdf13a
David Keller <dckeller1@gmail.com>
2018-02-27 23:06:49 -0700

    All links and routes function correctly

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,10 +1,20 @@
 class ProfilesController < ApplicationController
 	skip_before_action :require_login
 
+	## This is the profile page for SSServices ##
+	def index
+		@user = User.find(2)
+		@skill = Skill.where(user_id: 2)
+		@slot = Slot.where(user_id: 2)
+	end
+
+	## Profile pages for everyone else ##
 	def show
 		@user = User.find(params[:id])
 		@users = User.all
 		@skill = Skill.where(user_id: current_user.id)
 		@slot = Slot.where(user_id: current_user.id)
 	end
+
+
 end
