84ccc7599c019bc4d7a227f66774fcfd63619d21
David Keller <dckeller1@gmail.com>
2018-02-28 21:23:28 -0700

    Starting over again

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -10,6 +10,7 @@ class ProfilesController < ApplicationController
 
 	## Profile pages for everyone else ##
 	def show
+		@user = User.find(params[:id])
 		@users = User.all
 		@skill = Skill.where(user_id: current_user.id)
 		@slot = Slot.where(user_id: current_user.id)
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,9 @@ Rails.application.routes.draw do
 
 	resources :conversations do 
 		resources :messages
-	end 
+	end
+
+	resources :slots 
 
 	resources :skills
 
