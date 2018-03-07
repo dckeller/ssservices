c16191efbb1d7daa344891043755db465545d7fb
David Keller <dckeller1@gmail.com>
2018-03-07 14:17:12 -0700

    Cleaning up About Us

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -3,9 +3,6 @@ class ProfilesController < ApplicationController
 
 	## This is the profile page for SSServices ##
 	def index
-		@user = User.find(1)
-		@skill = Skill.where(user_id: 1)
-		@slot = Slot.where(user_id: 1)
 	end
 
 	## Profile pages for everyone else ##
